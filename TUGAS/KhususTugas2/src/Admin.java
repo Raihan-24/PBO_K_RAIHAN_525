12433b54b2f1e8688cadbeb89dcf30a1de1e6915
Raihan-24 <alfatahraihan182@gmail.com>
2025-05-07 17:50:20 +0700

    push something

--- TUGAS/KhususTugas2/src/Admin.java
+++
@@ -1,9 +0,0 @@
-// Kelas Admin
-class Admin {
-    private String username = "Admin525";
-    private String password = "Password525";
-
-    public boolean login(String inputUsername, String inputPassword) {
-        return username.equals(inputUsername) && password.equals(inputPassword);
-    }
-}
